5ba4e922f84ed0cea21149d52552251ff7dcb356
AhsDevelopers <beneva1@FVFGV3NAQ05P.local>
2022-12-02 10:28:24 +0100

    overbodige code wegnemen

---
+++ js/slider-team.js
@@ -0,0 +1,34 @@
+/**
+ * Create Team Glider
+ */
+new Glider(document.getElementById('glider-team'), {
+  slidesToShow: 2,
+  draggable: true, 
+  dots: '#dots-team',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 1501,
+      settings: {
+        slidesToShow: 5,
+      },
+    },
+  ]
+});
--- js/slider-workshops.js
+++ js/slider-workshops.js
@@ -5,6 +5,10 @@ new Glider(document.getElementById('glider-workshops'), {
   slidesToShow: 1,
   draggable: true,
   dots: '#dots-workshops',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  },
   responsive: [
     {
       breakpoint: 768,
@@ -30,40 +34,5 @@ new Glider(document.getElementById('glider-workshops'), {
     //     slidesToShow: 5,
     //   },
     // },
-  ],
-});
-
-/**
- * Create Team Glider
- */
-new Glider(document.getElementById('glider-team'), {
-  slidesToShow: 2,
-  draggable: true,
-  dots: '#dots-team',
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 1201,
-      settings: {
-        slidesToShow: 4,
-      },
-    },
-    {
-      breakpoint: 1501,
-      settings: {
-        slidesToShow: 5,
-      },
-    },
   ],
 });
